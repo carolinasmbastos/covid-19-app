d6fb305e894aca598c21f6a8d393a0b03687be75
carolinasmbastos <carolinasmbastos@gmail.com>
2020-03-30 14:25:25 -0700

    Fix indenting

--- src/components/chart/CountryChart.js
+++ src/components/chart/CountryChart.js
@@ -1,73 +1,65 @@
-import React from 'react'
-import { ResponsiveBar } from '@nivo/bar'
-
-// make sure parent container have a defined height when using
-// responsive component, otherwise height will be 0 and
-// no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
-
-
+import React from "react";
+import { ResponsiveBar } from "@nivo/bar";
 
 const CountryChart = ({ data /* see data tab */ }) => (
-    <ResponsiveBar
-        data={data}
-        keys={[ 'confirmed' ]}
-        indexBy="lastUpdate"
-        margin={{ top: 50, right: 60, bottom: 50, left: 60 }}
-        padding={0.3}
-        colors=  '#3f51b5'
-        borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'days',
-            legendPosition: 'middle',
-            legendOffset: 32
-        }}
-        axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'Confirmed cases',
-            legendPosition: 'middle',
-            legendOffset: -50
-        }}
-        enableLabel={false}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        labelTextColor={{ from: 'color', modifiers: [ [ 'brighter', 2.6 ] ] }}
-        legends={[
-            {
-                dataFrom: 'keys',
-                anchor: 'top',
-                direction: 'row',
-                justify: false,
-                translateX: 0,
-                translateY: -30,
-                itemsSpacing: 2,
-                itemWidth: 100,
-                itemHeight: 20,
-                itemDirection: 'left-to-right',
-                itemOpacity: 0.85,
-                symbolSize: 20,
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemOpacity: 1
-                        }
-                    }
-                ]
+  <ResponsiveBar
+    data={data}
+    keys={["confirmed"]}
+    indexBy="lastUpdate"
+    margin={{ top: 50, right: 60, bottom: 50, left: 60 }}
+    padding={0.3}
+    colors="#3f51b5"
+    borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+    axisTop={null}
+    axisRight={null}
+    axisBottom={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: "days",
+      legendPosition: "middle",
+      legendOffset: 32
+    }}
+    axisLeft={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: "Confirmed cases",
+      legendPosition: "middle",
+      legendOffset: -50
+    }}
+    enableLabel={false}
+    labelSkipWidth={12}
+    labelSkipHeight={12}
+    labelTextColor={{ from: "color", modifiers: [["brighter", 2.6]] }}
+    legends={[
+      {
+        dataFrom: "keys",
+        anchor: "top",
+        direction: "row",
+        justify: false,
+        translateX: 0,
+        translateY: -30,
+        itemsSpacing: 2,
+        itemWidth: 100,
+        itemHeight: 20,
+        itemDirection: "left-to-right",
+        itemOpacity: 0.85,
+        symbolSize: 20,
+        effects: [
+          {
+            on: "hover",
+            style: {
+              itemOpacity: 1
             }
-        ]}
-        animate={true}
-        motionStiffness={90}
-        motionDamping={15}
-    />
-)
+          }
+        ]
+      }
+    ]}
+    animate={true}
+    motionStiffness={90}
+    motionDamping={15}
+  />
+);
 
-export default CountryChart
+export default CountryChart;
